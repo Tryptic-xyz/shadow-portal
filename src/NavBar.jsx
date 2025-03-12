b2a7c6f8c2fb19c3ee1498f4237993c1f02ef314
Matt Will <matthew.will.dev@gmail.com>
2025-03-11 20:46:18 -0500

    updated shadow logo path

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,5 @@
 
+import logo from "/icons/shadow-logo.svg"
 import ConnectWallet from "./ConnectWallet.jsx";
 import Menu from "./Menu.jsx";
 
@@ -6,7 +7,7 @@ function NavBar({ setIsLoggedIn }) {
   return (
     <div className=" flex justify-between w-full py-4">
       <a href="/" rel="noopener noreferrer">
-        <img src="/images/shadow-logo.svg" alt="Shadow Portal Logo" className="h-10 md:h-12" />
+        <img src={logo} alt="Shadow Portal Logo" className="h-10 md:h-12" />
       </a>
       <div className="flex gap-3">
         <ConnectWallet setIsLoggedIn={setIsLoggedIn} />
